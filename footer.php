73441e07a4a96698f7aba873075d136a5e133ca8
Sedakat EKMEKÇİ <sedakatekmekci16@gmail.com>
2022-05-10 09:49:44 +0300

    create project

---
+++ footer.php
@@ -0,0 +1,48 @@
+<div class="footer container-fluid ">
+    <div class="card">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-4">
+                <div class="footer-text pull-left">
+                    <div class="logo d-flex">
+                    	<img src="assest\images\Scarlogo.png">
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-2">
+               
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-2">
+                <h5 class="heading">Services</h5>
+                <ul>
+                    <li>Operational Leasing</li>
+                    <li>Corparate Rental</li>
+                    <li>Induvidual Rental</li>
+                    <li>Support</li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-2">
+                <h5 class="heading">Company</h5>
+                <ul class="card-text">
+                    <li>About Us</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                    <li>Join Us</li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-2">
+                <h5 class="heading">Policys</h5>
+                <ul class="card-text">
+                   <li>Terms of Use</li>
+                    <li>Privacy Policy</li>
+                    <li>Cookie Policy</li>
+                </ul>
+                </div>
+        </div>
+            
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,44 @@
+<div class=" header container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div  class="logo navbar-brand " >
+					<img src="assest\images\Scarlogo.png">
+				</div> 
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							 <a class="nav-link" href="#">Our Services</a>
+						</li>
+						<li class="nav-item">
+							 <a class="nav-link" href="#">Cars</a>
+						</li>
+						<li class="nav-item">
+							 <a class="nav-link" href="#">Offers</a>
+						</li>
+						
+					</ul>
+					<ul class="navbar-nav ml-md-auto">
+						<li class="nav-item dropdown">
+							 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown">My Account</a>
+							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+								 <a class="dropdown-item" href="#">Settings</a> <a class="dropdown-item" href="#">social connect</a> <a class="dropdown-item" href="#">Privacy</a>
+								<div class="dropdown-divider">
+								</div> <a class="dropdown-item" href="#">Logout</a>
+							</div>
+						</li>
+						<li>
+							<li class="nav-item">
+							 <a class="nav-link" href="#">Login</a>
+						</li>
+						</li>
+					</ul>
+					
+				</div>
+			</nav>
+		</div>
+	</div>
+</div>
