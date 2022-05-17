83da9adea77d15b2c8d6552166936ecb47b1f4ef
Sedakat EKMEKÇİ <sedakatekmekci16@gmail.com>
2022-05-17 11:58:33 +0300

    update admin page

---
+++ admin_header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="assets\css\style.css">
+<link rel="stylesheet" type="text/css" href="assets\css\index.css">
+<link rel="stylesheet" type="text/css" href="assets\css\cars.css">
+<link rel="stylesheet" type="text/css" href="assets\css\admin.css">
+<link rel="stylesheet" type="text/css" href="assets\css\bootstrap\bootstrap.min.css">
+
+
+</head>
+<body>
+		<div class="head container">
+		<div class="row">	<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="assets\images\Scarlogo.png" alt="">
+    </a>
+  </div>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="cars.php">Cars</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Services.php">Services</a>
+        </li>
+        
+        <li class="nav-item">
+         <a class="nav-link" href="admin_index.php">Login</a>
+    	</li>
+	</ul>
+    </div>
+  </div>
+</nav>
+</div>
+</div>
