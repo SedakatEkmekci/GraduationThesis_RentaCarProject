53052ba97def828a95ac650829b945a0b3042f13
Sedakat EKMEKÇİ <sedakatekmekci16@gmail.com>
2022-05-17 12:43:25 +0300

    Change Ademin Page

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -4,7 +4,6 @@
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="assets\css\style.css">
 <link rel="stylesheet" type="text/css" href="assets\css\index.css">
-<link rel="stylesheet" type="text/css" href="assets\css\cars.css">
 <link rel="stylesheet" type="text/css" href="assets\css\admin.css">
 <link rel="stylesheet" type="text/css" href="assets\css\bootstrap\bootstrap.min.css">
 
@@ -21,17 +20,17 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="admin.php">Admin</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="cars.php">Cars</a>
+          <a class="nav-link" href="cars.php">Customers</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Services.php">Services</a>
+          <a class="nav-link" href="Services.php">Offers</a>
         </li>
         
         <li class="nav-item">
-         <a class="nav-link" href="admin_index.php">Login</a>
+         <a class="nav-link" href="logout.php">Logout</a>
     	</li>
 	</ul>
     </div>
--- admin/admin_index.php
+++ admin/admin_index.php
@@ -2,21 +2,33 @@
 include "admin_header.php";
 ?>
 
-<div align = "center">
- <form method="POST" action="admin_login.php">
+<div align="center">
+   <div class="card" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="assets\images\Scarlogo.png" class="img-fluid " alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <form method="POST" action="admin_login.php">
   <div align="center">
     <div class="container">
          <form action = "" method = "post">
-            <label>User Name</label><br>
+            <label>Username</label><br>
             <input type = "text" name = "username" required /><br><br>
             <label>Password </label><br>
             <input type = "password" name = "password" required/><br><br>
             <button class="Submit">Submit </button>
                </form>
+      </div>
     </div>
-   </div>
+  </div>
+</div>
+ </div>
+  </div>
 </div>
  
+
   
      
 
